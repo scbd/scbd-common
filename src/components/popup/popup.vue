b700a77563cf225d0060ecf7479eb217cfeb1a74
Rodrigo Elias <rodrigo.elias@cbd.int>
2023-10-10 10:57:53 -0400

    popup structure

--- src/components/popup/popup.vue
+++ src/components/popup/popup.vue
@@ -1,15 +1,44 @@
-<template />
-
+<template>
+  <div class="modal fade" :class="{ show: showModal }" id="popUp" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">CBD</h5>
+          <button type="button" class="close" @click="closeModal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Questionnaire</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" @click="closeModal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
-import gtag from './services/util/gtag.js';
+import gtag from '../../services/util/gtag';
 
 export default {
     name: 'popUp',
-    props: {
-        endDate: Date,
-        url: String,
+    data() {
+        return {
+            showModal: false,
+        }
     },
+    mounted() {
+    setTimeout(() => {
+            this.showModal = true;
+        }, 100);
+    },
+    methods: {
+        closeModal() {
+            this.showModal = false;
+        },
+    }
 }
 </script>
 
