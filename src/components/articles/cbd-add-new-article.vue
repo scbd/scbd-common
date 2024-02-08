2a51fb77ea40a46241080970437dd0dac677d53b
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-02-08 18:53:54 +0500

    update add-new-article component

--- src/components/articles/cbd-add-new-article.vue
+++ src/components/articles/cbd-add-new-article.vue
@@ -1,5 +1,5 @@
 <template>
-     <a :href="newArticleUrl" :target="target">
+        <a :href="newArticleUrl" :target="target">
         <slot>
             <span v-if="!id"><i class="fa fa-plus"></i> Add Content</span>
             <span v-if="id" ><i class="fa fa-edit"></i> Edit Content</span>
@@ -8,42 +8,37 @@
     </a>
 </template>
 
-<script>
-    export default {
-        name:'cbdAddNewArticle',
-        props: {
-            tags 		: { type: Array  , required: false, default:[]           }, // [] of tag id's
-            customTags 	: { type: Array  , required: false, default:[]           }, // [] of customTag id's
-            adminTags 	: { type: Array  , required: false, default:[]           }, // [] of adminTag text
+<script setup>
+import { ref, computed } from 'vue';
+const { tags, customTags, adminTags, target, id } = defineProps({
+            tags 		: { type: Array  , required: false, default: () => []           }, // [] of tag id's
+            customTags 	: { type: Array  , required: false, default: () => []           }, // [] of customTag id's
+            adminTags 	: { type: Array  , required: false, default: () => []           }, // [] of adminTag text
             target      : { type: String , required: false, default: '_blank'    },
             id          : { type: String , required: false, default: undefined   },
-        },
-        computed: {
-            newArticleUrl : function(){
+        });
 
-                const domain = window.location.hostname.replace(/[^\.]+\./, '');
-				let baseUrl = 'https://www.cbd.int/management'
+const newArticleUrl = computed(() => {
+ const domain = window.location.hostname.replace(/[^\.]+\./, '');
+ let baseUrl = 'https://www.cbd.int/management';
 
-				if(domain=='localhost' || domain == 'cbddev.xyz')
-            		baseUrl = 'https://oasis.cbddev.xyz';
+ if (domain === 'localhost' || domain === 'cbddev.xyz') baseUrl = 'https://oasis.cbddev.xyz';
 
 				const queryString = [];
-                if(!this.id){
-                    if(this.tags?.length)
-                        queryString.push('tags='		+ this.tags.map(encodeURIComponent).join(','))
-                    if(this.customTags?.length)
-                        queryString.push('customTags='	+ this.customTags.map(encodeURIComponent).join(','))
-                    if(this.adminTags?.length)
-                        queryString.push('adminTags='	+ this.adminTags.map(encodeURIComponent).join(','))
+                if(!id){
+                    if(tags?.length)
+                        queryString.push('tags='		+ tags.map(encodeURIComponent).join(','))
+                    if(customTags?.length)
+                        queryString.push('customTags='	+ customTags.map(encodeURIComponent).join(','))
+                    if(adminTags?.length)
+                        queryString.push('adminTags='	+ adminTags.map(encodeURIComponent).join(','))
                 }
 
                 queryString.push('returnUrl=' + encodeURI(window.location.href));
 
-                if(!this.id)
-                    return `${baseUrl}/articles/new?${queryString.join('&')}`
-                
-                return  `${baseUrl}/articles/${encodeURIComponent(this.id)}/edit?${queryString.join('&')}`;
-            }
-        }
-    }
+                if (!id)
+                    return `${baseUrl}/articles/new?${queryString.join('&')}`;
+
+                return `${baseUrl}/articles/${encodeURIComponent(id)}/edit?${queryString.join('&')}`;
+});
 </script>
