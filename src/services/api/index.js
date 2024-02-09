db6ed4508e902878c59373a1d3e522522fc90318
Yuan Li <liyuan.montreal@gmail.com>
2024-02-09 16:22:51 -0500

    migrate api files

---
+++ src/services/api/index.js
@@ -0,0 +1,34 @@
+
+// import ArticlesApi            from '@/api/articles'
+// import CountriesApi           from '@/api/countries'
+// import KmDocumentsApi         from '@/api/km-storage'
+// import KmWorkflowsApi         from '@/api/km-workflows'
+// import RealmConfigurationApi  from '@/api/realm-configuration'
+// import SolrIndexApi           from '@/api/solr-index'
+// import ThesaurusApi           from '@/api/thesaurus'
+
+import ArticlesApi            from './articles'
+import CountriesApi           from './countries'
+import KmDocumentsApi         from './km-storage'
+import KmWorkflowsApi         from './km-workflows'
+import RealmConfigurationApi  from './realm-configuration'
+import SolrIndexApi           from './solr-index'
+import ThesaurusApi           from './thesaurus'
+
+const articles           = new ArticlesApi          (context);
+const countries          = new CountriesApi         (context);
+const kmDocuments        = new KmDocumentsApi       (context);
+const kmWorkflows        = new KmWorkflowsApi       (context);
+const realmConfiguration = new RealmConfigurationApi(context);
+const solrIndex          = new SolrIndexApi         (context);
+const thesaurus          = new ThesaurusApi         (context);
+
+export const Api = {
+  articles          ,
+  countries         ,
+  kmDocuments       ,
+  kmWorkflows       ,
+  realmConfiguration,
+  solrIndex         ,
+  thesaurus         ,
+}
---
+++ src/services/api/km-workflows.js
@@ -0,0 +1,75 @@
+
+import ApiBase, { tryCastToApiError } from './api-base';
+
+export default class KmWorkflowsApi extends ApiBase
+{
+    constructor(options) {
+        super(options);
+    }
+    
+    
+    async getWorkflowHistory({q, f, s, sk, l , c, fo, ag })  {
+        const params = toUrlParams( {q, f, s, sk, l , c, fo, ag });
+
+        return this.http.get(`/api/v2013/workflows`, { params })
+                        .then(res => res.data)
+                        .catch(tryCastToApiError);
+    }
+
+    async getWorkflow(workflowId)  {
+        if(!isValid(workflowId)) throw Error(`invalid value for workflowId`);
+      
+        return this.http.get(`/api/v2013/workflows/${encodeURIComponent(workflowId)}`)
+                        .then(res => res.data)
+                        .catch(tryCastToApiError);
+    }
+
+    async getBatchWorkflowDetails(batchId){ //:Promise<EKmDocumentsBatchWorkflow>  {
+        if(!isValid(batchId)) throw Error(`invalid value for batchId`);
+
+        return this.http.get(`/api/v2013/workflows/batches/${encodeURIComponent(batchId)}`)
+                        .then(res => res.data)
+                        .catch(tryCastToApiError);
+    }
+
+    async updateActivity(id, activityName, body) {  
+        if(!isValid(id)) throw Error(`invalid value for id`);
+        if(!isValid(activityName)) throw Error(`invalid value for activityName`);
+        if(!isValid(body)) throw Error(`invalid value for body`);
+
+        return this.http.put(`/api/v2013/workflows/" + id + "/activities/" + activityName`, body)
+                        .then(res => res.data)
+                        .catch(tryCastToApiError);
+    }
+
+    async updateBatchActivity(id, activityName, body) {
+        if(!isValid(id)) throw Error(`invalid value for id`);
+        if(!isValid(activityName)) throw Error(`invalid value for activityName`);
+        if(!isValid(body)) throw Error(`invalid value for body`);
+
+        return this.http.put("/api/v2013/workflows/batches/" + id + "/activities/" + activityName, body)
+                        .then(res => res.data)
+                        .catch(tryCastToApiError);
+    
+    }
+
+    async cancelWorkflow(id) {        
+        if(!isValid(id)) throw Error(`invalid value for id`);
+        const params = { 'action': 'cancel' };
+
+        return this.http.delete(`/api/v2013/workflows/${id}`, params)
+                        .then(res => res.data)
+                        .catch(tryCastToApiError);
+
+    }
+
+    async cancelBatch(batchId) {
+        if(!isValid(batchId)) throw Error(`invalid value for batchId`);
+        const params = { 'action': 'cancel' };
+
+        return this.http.delete(`/api/v2013/workflows/batches/${batchId}`, params)
+                        .then(res => res.data)
+                        .catch(tryCastToApiError);
+
+    }
+}
