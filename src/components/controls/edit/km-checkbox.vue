a7d3549fdb40ef154fd0e473638dcd0f7d77ab93
Tahmeed Shah <58165529+tahmeedshah@users.noreply.github.com>
2024-02-22 14:45:32 +0500

    km-checkbox component

---
+++ src/components/controls/edit/km-checkbox.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <div class="form-check">
+        <input type="checkbox" class="form-check-input"
+            v-model="isChecked"
+            :id="fieldId" 
+            :checked="checked"  
+        />
+        <label :for="fieldId" class="form-check-label">            
+            {{ label }}
+        </label>
+    </div>
+</template>
+    
+<script setup>
+    const { label, fieldId } = defineProps ({
+            label: { type: String, required: true },
+            fieldId: { type: String, required: true },
+            checked: { type: Boolean, default: false },
+        })
+    const isChecked = defineModel();
+</script>
