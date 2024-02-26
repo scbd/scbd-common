f502d3ef7f893d8d758a4a33e411e16096007364
Yuan Li <liyuan.montreal@gmail.com>
2024-02-26 17:44:20 -0500

    add basic date picker

---
+++ src/components/km-date.vue
@@ -0,0 +1,28 @@
+<script setup>
+    const model = defineModel()
+
+    const props = defineProps({
+        modelValue: {
+            type    : [ Object ],
+            required: true,
+            default:""
+        },
+        'type':String
+      
+    });
+    const emit  = defineEmits(['update:modelValue']);
+
+    const onChange = (event) => {
+         emit('update:modelValue', event.target.value);
+    };
+</script>
+
+<template>
+  <Input    id = "dateSelector"
+            v-model="model"       
+            :type=type
+            placeholder="Select date"
+            :disabled="disabled"
+            @input="onChange"               
+    /> 
+</template>
