69327d3660952931e52293d22943543eb2cb566c
Blaise Fonseca <blaise.fonseca@un.org>
2024-02-28 13:34:46 -0500

    add date controls to preview

--- index.vue
+++ index.vue
@@ -3,17 +3,30 @@
 
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=BenchNine:300,400,600,900" media="all" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css,npm/@scbd/www-css@1.0.1/dist/style.min.css,
-      npm/animate.css@3.5.2/animate.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css,npm/@scbd/www-css@1.0.1/dist/style.min.css,npm/animate.css@3.5.2/animate.min.css">
 
   <link rel="stylesheet" href="https://www.cbd.int/app/template-phoenix-overrides.css">
   <link rel="stylesheet" href="https://www.cbd.int/app/css/bootstrap-migrate-3-to-4.css">
 
     <ScbdHeader></ScbdHeader>
-    <div style="margin-top:20px;margin-bottom:50px">
-      <h1>Dev Applications</h1>
-      <cop-count-down cop-title="COP 15" :end-date="endDate" :url="copUrl"></cop-count-down>
+    <div class="container">
+      <legend>Controls usage</legend>
+      <hr/>
+      <div class="row">
+        <div class="col-3">
+          <h3>COP Countdown</h3>
+          <cop-count-down cop-title="COP 15" :end-date="endDate" :url="copUrl"></cop-count-down>
+        </div>
+        <div class="col-3">
+          <h3>Date Selector</h3> {{ dateValue }}
+          <date-selector  class="test" v-model="dateValue"></date-selector>
+        </div>
+      </div>
     </div>
+    <br>
+    <br>
+    <br>
+    <br>
     <ScbdFooter></ScbdFooter>
   </div>
 </template>
@@ -22,13 +35,15 @@
   // ToDo path will change after rollup fixes
   import ScbdHeader from './src/components/cbd-nav/header.vue'
   import ScbdFooter from './src/components/cbd-nav/footer.vue'
-  import CopCountDown from './src/components/countdown/cop-count-down.vue'; 
-
+  import CopCountDown from './src/components/countdown/cop-count-down.vue';
+  import dateSelector from  './src/components/inputs/dateSelector.vue'
+  import { ref } from 'vue';
 
   //<--------------- Testing Area ----------------------> 
   //Cop-count-down testing entries
   const endDate = new Date('2024-04-20');
   const copUrl = 'https://www.unep.org/un-biodiversity-conference-cop-15';
+  const dateValue = ref('2024-02-06');
 
 </script>
 
