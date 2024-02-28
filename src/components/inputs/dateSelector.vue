f13cc5705faad107deaef8fc6871c1c9586c9f23
Yuan Li <65098066+liyuanmontreal@users.noreply.github.com>
2024-02-28 13:35:30 -0500

    CHM288 migrate dateselector  (#21)
    a simple date picker component implemented by Vue
    user can set type and disabled parameter. 
    type:        "date" | "month" , "date" is default value
    disabled:  true| false
    
    here is a example to use it
    
    
    <img width="370" alt="Screenshot 2024-02-28 113955"
    src="https://github.com/scbd/scbd-common/assets/65098066/a45cc609-dd6e-473a-9371-ce79185f1cb5">
    
    ---------
    
    Co-authored-by: Blaise Fonseca <blaise.fonseca@un.org>

---
+++ src/components/inputs/dateSelector.vue
@@ -0,0 +1,25 @@
+<template>    
+    <input    
+        v-model="model"
+        :class="$attrs.class"
+        :id="$attrs.id"
+        :required="$attrs.required" 
+        :placeholder="$attrs.placeholder"
+        :disabled="$attrs.disabled"
+        :type="type" /> 
+</template>
+
+<script setup>
+    //TODO: check if browser support datepicker
+    //https://stackoverflow.com/questions/10193294/how-can-i-tell-if-a-browser-supports-input-type-date
+    const model = defineModel();    
+    const props = defineProps({
+        type:{
+            type:String,
+            default:"date",
+            validator(value) { 
+                return ['date', 'month'].includes(value)
+            }
+        }   
+    });
+</script>
