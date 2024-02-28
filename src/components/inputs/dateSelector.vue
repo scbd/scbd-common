5eb828b590b16cdd587b442480f6afe6e141c2c7
Yuan Li <liyuan.montreal@gmail.com>
2024-02-28 13:03:03 -0500

    fix code review issue

--- src/components/inputs/dateSelector.vue
+++ src/components/inputs/dateSelector.vue
@@ -1,3 +1,13 @@
+<template>
+    <input    id = "dateSelector"
+              v-model="model"       
+              :type="type"
+              :placeholder="placeholder"
+              :disabled="disabled"
+              @input="onChange"               
+    /> 
+</template>
+
 <script setup>
     //TODO: check if browser support datepicker
     //https://stackoverflow.com/questions/10193294/how-can-i-tell-if-a-browser-supports-input-type-date
@@ -5,15 +15,25 @@
 
     const props = defineProps({
         modelValue: {
-            type    : [ Object ],
+            type    : String,
             required: true,
             default:""
         },
-        'type':{
+        type:{
             type:String,
-            default:"date"
-        }
-      
+            default:"date",
+            validator(value) { 
+                return ['date', 'month'].includes(value)
+            }
+        },
+        placeholder:{
+            type:String,
+            default:"Select Date"
+        },
+        disabled:{
+            type:Boolean,
+            default:false
+        }      
     });
     const emit  = defineEmits(['update:modelValue']);
 
@@ -21,13 +41,3 @@
          emit('update:modelValue', event.target.value);
     };
 </script>
-
-<template>
-  <input    id = "dateSelector"
-            v-model="model"       
-            :type=type
-            placeholder="Select date"
-            :disabled="disabled"
-            @input="onChange"               
-    /> 
-</template>
