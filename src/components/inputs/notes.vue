179a830dbea271fec8135e910cb2a85da5e424f0
Yuan Li <liyuan.montreal@gmail.com>
2024-03-05 16:25:32 -0500

    fix code review issues

--- src/components/inputs/notes.vue
+++ src/components/inputs/notes.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <textarea            
+        <textarea   class="form-control"         
             v-model="newNote"           
             :id="$attrs.id"
             :class="$attrs.class"
             :required="$attrs.required" 
             :placeholder="$attrs.placeholder"
             :disabled="$attrs.disabled"
-            :rows="$attrs.rows" 
+            :rows="rows" 
             :cols="$attrs.cols" 
             @change="save" >
         </textarea>       
@@ -15,94 +15,59 @@
         <table class="table table-striped" v-if="notes?.length">
             <tr v-for="(item, index) in notes" :key="index">
                 <td>{{item}}  </td>   
-                <td style="width:10px;"><span class="btn" @click="remove(index)"><i class="fa fa-trash-o icon"></i></span></td>
-            </tr>
+                <td ><span class="btn float-end" @click="remove(index)"><i class="fa fa-trash-o icon"></i></span></td>
+              </tr>        
         </table>   
     </div> 
 </template>
 
 <script setup>
-   import { ref, computed } from 'vue'
-   import {useUser} from '../../services/composables/useAuth'
-  
-   let lastNote = '';
-   const newNote = ref();
-   const user = useUser();
+    import { ref, computed } from 'vue'
+    import {useUser} from '../../services/composables/useAuth'
+    import {format} from '../../services/filters/datetime'
 
-   const model = defineModel({type:String, required:true});
-   
+    const model = defineModel({type:String, required:true});
+
+    const props = defineProps({
+        rows:{
+            type:Number,
+            default:4    
+        }
+    });
+  
+    // using for prevent edit history list
+    let lastNote = '';
+    const newNote = ref("");
+    
+    // user info will be used for generate new note
+    const user = useUser();
+ 
+   // only get history lists
    let notes = computed(()=>{
         if(model.value)
             return  (JSON.parse(model.value)).filter(e=>e!=lastNote);
         return [];
    }); 
    
-   const remove=(index)=>{  
-        for (var i = 0; i < notes.value.length; i++) {        
-            if(notes.value[i] == notes.value[index]){             
-                notes.value.splice(i, 1);        
-            }
-        }  
+    // delete history note
+    const remove=(index)=>{   
+        notes.value.splice(index, 1);
         save();  
-   };
+    };
 
-    const save=()=>{   
-        const timestamp = new Intl.DateTimeFormat('en-US', {
-            year: 'numeric',
-            month: 'short',
-            day: '2-digit',
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric',
-            hour12: true
-        }).format(new Date());
-        const lNotes = notes.value;
-        if(lNotes[lNotes.length-1] == lastNote)
-             lNotes.splice(lNotes.length-1, 1);
-            
-        var newLNote = newNote.value? "[ " +  user.value.name  + " | " + timestamp + " ] - " + newNote.value.trim():"";  
+    // save whole note list (history notes list + new note)
+    const save=()=>{  
+        // prevent edit history notes list
+        const lNotes = notes.value; 
+        if(lNotes.at(-1) == lastNote)
+             lNotes.pop();
+   
+        // generate new note
+        const timestamp = format(new Date(), "FF");  
+        var newLNote = newNote.value ? `[ ${user.value.name} | ${timestamp} ] - ${newNote.value.trim()}` : "";
         lastNote =newLNote; 
        
+        // sending history notes + new note to parent page
         model.value = JSON.stringify([...lNotes, newLNote]);
     };
 </script>
-<style>
-    textarea:focus {
-        box-shadow: 0 0 0 0.15rem rgb(13 110 253 / 25%);
-    }
-
-    textarea::placeholder {
-        color: #999;
-        opacity: 1;
-        font-size: .8rem;
-    } 
-
-    textarea {
-        resize: none;
-        display:inline-block;
-        width:100%;
-    }
-
-    table { 
-        width:100%;
-    }
-
-    table, td , tr{
-        padding: 0;
-        margin: 0;
-        vertical-align: top;
-    }
-
-    tr:nth-child(odd)   {
-        background-color:#E3E3E3
-    }
-    tr:nth-child(odd)  span {
-        background-color:#E3E3E3
-    }
-    tr:nth-child(even)   span {
-        background-color:#EFEFEF
-    }
-    tr:nth-child(even)    {
-        background-color:#EFEFEF
-    }
-</style>
