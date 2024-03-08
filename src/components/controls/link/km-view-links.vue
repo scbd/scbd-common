604ce3f10191d11a6cf27c9efb37fce05d213708
Yuan Li <liyuan.montreal@gmail.com>
2024-03-08 13:11:18 -0500

    update link-editor.vue , add overflow to modal window

---
+++ src/components/controls/link/km-view-links.vue
@@ -0,0 +1,6 @@
+<template>
+
+<v-mode will take list of links
+    <table to show links
+    <raise onDelete event when delete button is click
+</template>
--- src/components/controls/link/link-editor.vue
+++ src/components/controls/link/link-editor.vue
@@ -1,94 +1,83 @@
 <template> 
-    <div v-if="modalOpen" class="modal" > 
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header"> 
-            <h3 class="modal-title"><slot>Editing link</slot></h3>  
-          </div>            
-          <div class="modal-body"  >				
-            <div class="alert alert-info">Please provide the URL of the website (e.g.
-                <a rel="noopener" translation-url target="_blank" href="http://www.cbd.int">https://www.cbd.int</a> )and the name of the website (e.g. "The Convention on Biological Diversity").
-            </div>					
-            <form>
-              <div class="mb-3" >
-                  <label class="col-form-label" for="url">Url <span style="color: #e32"> *</span></label>
-                  <div class="help-block">Protocol is required (https:// or http://)</div>	                  	
-                  <p v-if=urlErrorMsgShow ><span style="color: #e32"> Please provide valid URL</span></p>				
-                  <input class="form-control"  id ="url" :value="link.url" type="url" style="width:98%" ng-model="editor.url"  placeholder=" https://www." />
-              </div>
-              <div class="mb-3" >
-                  <label for="name" class="col-form-label" >Name</label>
-                  <input class="form-control" id="name" :value="link.name" type="text" style="width:98%"  placeholder="example:SCBD website" />
-              </div>
-              <div class="mb-3">
-                  <label for="language" class="col-form-label">Language <span style="color: #e32"> *</span></label>   
-                  <p v-if=languageErrorMsgShow ><span style="color: #e32"> Please select Language </span></p>
-                    <select class="form-select"  name="language" id="language" :value="link.language||''"> 
-                      <option value="ar">Arabic</option>                 
-                      <option value="en">English</option>     
-                      <option value="cn">Chinese</option>
-                      <option value="fr">French</option>                 
-                      <option value="es">Spanish</option>
-                      <option value="ru">Russian</option>
-                  </select>
-              </div>
-            </form>
-          </div> 
-          <div class="modal-footer ">  
-            <button type="button" class="btn btn-secondary" @click="close" >Cancel</button>
-            <button type="button" class="btn btn-primary  " @click="save" >Save</button>         
-          </div>
+    <div id="linkEditorModal" v-if="modalOpen" class="modal fade show overflow-auto" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" style="padding-right: 17px; display: block;">
+        <div class="modal-dialog" role="document" >
+            <div class="modal-content">
+                <div class="modal-header">     
+                    <h3 class="modal-title"><slot name="modalTitle">Editing link</slot></h3>  
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"  @click="close" >
+                    <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                        <div class="alert alert-info">Please provide the URL of the website (e.g.
+                            <a rel="noopener" translation-url target="_blank" href="http://www.cbd.int">https://www.cbd.int</a> )and the name of the website (e.g. "The Convention on Biological Diversity").
+                        </div>					
+                        <form>
+                        <div class="mb-3" >
+                            <label class="col-form-label" for="url">Url <span style="color: #e32"> *</span></label><br/>
+                            <small class="help-block">Protocol is required (https:// or http://)</small>	               				
+                            <input class="form-control"  id ="url" v-model="link.url" type="url" style="width:98%"  placeholder=" https://www." />
+                            <p v-if="!isUrlValid" ><span style="color: #e32"> Please provide valid URL</span></p>
+                            </div>
+                        <div class="mb-3" >
+                            <label for="name" class="col-form-label" >Name</label>
+                            <input class="form-control" id="name" v-model="link.name" type="text" style="width:98%"  placeholder="example:SCBD website" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="language" class="col-form-label">Language <span style="color: #e32"> *</span></label>   
+                            <p v-if="!isLangValid" ><span style="color: #e32"> Please select Language </span></p>
+                                <select class="form-select"  name="language" id="language" v-model="link.language"> 
+                                <option value="ar">Arabic</option>                 
+                                <option value="en">English</option>     
+                                <option value="cn">Chinese</option>
+                                <option value="fr">French</option>                 
+                                <option value="es">Spanish</option>
+                                <option value="ru">Russian</option>
+                            </select>
+                        </div>
+                        </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" @click="close" >Cancel</button>
+                    <button type="button" class="btn btn-primary  " @click="save" >Save</button>  
+                </div>    
+            </div>        
         </div>
-      </div>
-    </div>    
+    </div>     
   </template>
   
   <script setup >
-    import { defineEmits, ref } from "vue"; 
-    import { lstring } from "../../../services/filters/lstring"
+    import { defineEmits, ref, computed } from "vue"; 
      
     const modalOpen = ref(false) ;
     const link = ref({});     
-    const emit = defineEmits(['close']);
-    const urlErrorMsgShow = ref(false);
-    const languageErrorMsgShow = ref(false);
   
+    const emit = defineEmits(['close']);
+
+    const isUrlValid  = computed(()=>{ return isValidHttpUrl(link.value.url)});
+    const isLangValid = computed(()=>{ 
+        return !(typeof (link.value.language)=== 'undefined' || link.value.lang == "" );  
+    });
+
+ 
     const show= (linkToEdit)=>{  
-        modalOpen.value = true;          
-        link.value = linkToEdit;   
-        languageErrorMsgShow.value = false;
-        urlErrorMsgShow.value = false;  
+        modalOpen.value = true;    
+        const  { url, name, language } = linkToEdit  
+        link.value = { url, name, language }; 
     }
 
     const close = () => {      
       modalOpen.value = false;  
-      emit("close", {});  
+      emit("close");  
     }  
 
-    const save = () =>{ 
-      const isValid = validate();
-   
-      if (isValid){
-        const newLink = { "url": document.getElementById("url").value , "name": document.getElementById("name").value , "language": document.getElementById("language").value  };
+    const save = () =>{  
+      if (isUrlValid.value && isLangValid.value){       
+        const newLink = { "url": link.value.url , "name": link.value.name , "language": link.value.language  };
         modalOpen.value = false;   
         emit("close", newLink);  
       }
-      else{
-        link.value= { "url": document.getElementById("url").value , "name": document.getElementById("name").value , "language": document.getElementById("language").value  };        
-      }
     }
-
-    const validate = () => {   
-        // check url validation                 
-        urlErrorMsgShow.value =!isValidHttpUrl(document.getElementById("url").value);       
-        
-        // check language validation              
-        languageErrorMsgShow.value  = document.getElementById("language").value ==""; 
-        
-        const isValid = !languageErrorMsgShow.value  && !urlErrorMsgShow.value;    
-        return isValid;  
-    }
-  
     
     const isValidHttpUrl =(string)=> {
         try {
@@ -99,25 +88,11 @@
         }
     }
 
-
     defineExpose({
         show
     })
   </script>
   
   
-  <style scoped>
-     .modal {
-      position: fixed;
-      z-index: 1;
-      top: 0;
-      left: 0;
-      width: 100%;
-      overflow: auto;
-      height: 100%;
-      background-color: rgba(0, 0, 0, 0.5); 
-      display: flex;
-      transition: opacity 0.3s ease; 
-    } 
-  </style>
+
   
