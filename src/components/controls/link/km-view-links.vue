7fa0c802435fbeab9f0710150907dcd88698ff71
Yuan Li <liyuan.montreal@gmail.com>
2024-03-11 10:20:21 -0400

    add "edit "

--- src/components/controls/link/km-view-links.vue
+++ src/components/controls/link/km-view-links.vue
@@ -2,8 +2,11 @@
     <table class="table table-striped" v-if="links?.length"> 
         <tbody>
             <tr  v-for="(item, index) in links" :key="index">  
-                <td>{{languages[item.language]}}  <a :href="item.url" >{{item.name}}</a></td>
-                <td class = "text-end"><span class="btn" @click="deleteLink(index)"><i class="fa fa-trash-o icon"></i></span></td>
+                <td>{{languages[item.language]}}  <a :href="item.url" >{{item.name}}</a></td>   
+                <td class = "text-end">
+                    <span class="btn m-1" @click="editLink(index)"><i class="fa fa-edit icon"></i></span>
+                    <span class="btn" @click="deleteLink(index)"><i class="fa fa-trash-o icon"></i></span>
+                </td>
            </tr>        
         </tbody>
     </table> 
@@ -14,7 +17,7 @@
   import { languages } from '../../../data/language'
 
   const model = defineModel({type:Array, required:true});
-  const emit = defineEmits(['deleteLink']);
+  const emit = defineEmits(['deleteLink'],['editLink']);
    
     let links = computed(()=>{
         if(model.value)
@@ -24,5 +27,9 @@
  
     const deleteLink = (index) =>{    
         emit("deleteLink", index);        
+    }
+
+    const editLink = (index) =>{    
+        emit("editLink", index);        
     }
 </script>
