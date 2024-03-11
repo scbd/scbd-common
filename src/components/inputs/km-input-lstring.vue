dc52552b2fb10fc057a37f9d22a5fc2b8b9abf80
Blaise Fonseca <blaise.fonseca@un.org>
2024-03-11 11:49:04 -0400

    new commit

---
+++ src/components/inputs/km-input-lstring.vue
@@ -0,0 +1,87 @@
+<template>
+  <div :id="$attrs.id" class="scbd-controls km-input-lstring mb-2">
+    <slot></slot>
+    <div
+      class="input-group mb-1"
+      v-for="locale in userLocales"
+      :key="locale"
+      :class="`km-input-lstring-${locale}`"
+    >
+      <input
+        type="text"
+        class="form-control"
+        aria-describedby="basic-addon2"
+        v-model="binding[locale]"
+        :dir="locale == 'ar' ? 'rtl' : 'ltr'"
+        @update:modelValue="emitChange"
+      />
+
+      <div class="input-group-append">
+        <button
+          class="btn btn-outline-primary"
+          type="button"
+          id="basic-addon2"
+          :data-bs-toggle="'tooltip'"
+          :data-bs-placement="'top'"
+          :title="getTerm(locale).title"
+        >
+          {{ locale.toUpperCase() }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch, onMounted, defineEmits, computed } from "vue";
+import { without } from "lodash";
+
+const props = defineProps({
+  locales: { type: Array, required: true },
+  modelValue: {
+    type: Object,
+    default: () => ({}),
+    required: true,
+  },
+  disabled: { type: Boolean, default: false },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const userLocales = computed(() => props.locales);
+const binding = ref(props.modelValue);
+
+const emitChange = (value) => {
+  emit("update:modelValue", value || binding.value);
+};
+
+const loadLanguages = () => {
+  props.locales?.forEach((e) => {
+    //TODO: call thesaurus service API
+  });
+};
+
+const getTerm = (term) => {
+  //TODO: call thesaurus service API
+  return { title: term };
+};
+
+watch(
+  () => props.locales,
+  (newVal, oldVal) => {
+    const deleted = without(oldVal, ...newVal);
+    if (deleted?.length) {
+      deleted.forEach((e) => {
+        binding.value[e] = undefined;
+      });
+      emitChange();
+    }
+    loadLanguages();
+  }
+);
+
+onMounted(() => {
+  binding.value = { ...props.modelValue };
+  loadLanguages();
+});
+</script>
