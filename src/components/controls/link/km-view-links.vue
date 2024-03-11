2174044b07da72584a702e5806afe0914e56064a
Yuan Li <liyuan.montreal@gmail.com>
2024-03-11 09:57:31 -0400

    display link list / emit delete

---
+++ src/components/controls/link/km-view-links.vue
@@ -0,0 +1,28 @@
+<template>
+    <table class="table table-striped" v-if="links?.length"> 
+        <tbody>
+            <tr  v-for="(item, index) in links" :key="index">  
+                <td>{{languages[item.language]}}  <a :href="item.url" >{{item.name}}</a></td>
+                <td class = "text-end"><span class="btn" @click="deleteLink(index)"><i class="fa fa-trash-o icon"></i></span></td>
+           </tr>        
+        </tbody>
+    </table> 
+</template>
+
+<script setup>
+  import { defineEmits, computed } from 'vue'
+  import { languages } from '../../../data/language'
+
+  const model = defineModel({type:Array, required:true});
+  const emit = defineEmits(['deleteLink']);
+   
+    let links = computed(()=>{
+        if(model.value)
+            return  model.value;
+        return [];
+    });   
+ 
+    const deleteLink = (index) =>{    
+        emit("deleteLink", index);        
+    }
+</script>
