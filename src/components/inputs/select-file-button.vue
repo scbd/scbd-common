e96afbbbb98c4e782756dd658a90593d5ce9abe2
Yuan Li <liyuan.montreal@gmail.com>
2024-03-18 16:10:00 -0400

    change select-file-button to composition api format

---
+++ src/components/inputs/select-file-button.vue
@@ -0,0 +1,35 @@
+<template>
+    <button    
+      class="btn btn-primary"
+      type="button"
+      v-bind="$attrs"
+      @click.prevent.stop="selectFile()">  
+      <slot name="default" ></slot>
+    </button> 
+</template>
+  
+<script setup>  
+  const emit = defineEmits(['onSelectFiles']);
+  const props = defineProps({ 
+    multiple: { type: Boolean, require: false, default: false },
+    accept: { type: String, require: false, default: '*/*' }
+  })  
+  
+  const selectFile = () =>{   
+    const fileInput = document.createElement('INPUT');
+  
+    fileInput.type = 'file';
+    fileInput.style.display = 'none';
+  
+    if (props.multiple) fileInput.setAttribute('multiple', props.multiple);
+    if (props.accept && props.accept !== '*/*') fileInput.setAttribute('accept', props.accept);
+  
+    fileInput.addEventListener('click', ($event) => $event.stopPropagation());    
+    fileInput.addEventListener('change', ({ target }) => { 
+      const files = [...target.files];  
+      emit('onSelectFiles', files);   
+    }) 
+    fileInput.click();   
+  }  
+ 
+  </script>
