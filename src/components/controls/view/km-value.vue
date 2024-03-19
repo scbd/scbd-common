1cc8913b4cae0922ab17d5a6f105f07b35eac374
pjacobsam <162193392+pjacobsam@users.noreply.github.com>
2024-03-19 13:16:27 -0400

    migrate kmvalue to sbcd common (#37)

---
+++ src/components/controls/view/km-value.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :id="$attrs.id" class="scbd-controls km-value form-control">
+      <slot>
+        {{ value }}
+      </slot>
+    </div>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue';
+
+  const props = defineProps({
+    value: { type: String, required: true }
+  });
+</script>
+
+<style scoped>
+  /* Add Bootstrap styling to match the form-control class */
+</style>
