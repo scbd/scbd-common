6134192ade99ad688937f250f30fdb190e7571e3
pjacobsam <162193392+pjacobsam@users.noreply.github.com>
2024-03-21 11:11:29 -0400

    Added preview-component (#44)

---
+++ src/components/controls/preview-component.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          {{ $attrs["card-header"] }}
+        </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-6" ref="wrap">
+              <slot name="left"></slot>
+            </div>
+            <div class="col-6">
+              <div class="callout callout-warning">
+                <code>
+                  <slot name="right"></slot>
+                </code>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
