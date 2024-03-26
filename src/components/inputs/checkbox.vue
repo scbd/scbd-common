fa6eeaf73f81f7718eeeb7ef631052b05e012b6f
Yuan Li <liyuan.montreal@gmail.com>
2024-03-26 10:43:20 -0400

    migrate checkbox from ORT , change preview page

--- src/components/inputs/checkbox.vue
+++ src/components/inputs/checkbox.vue
@@ -1,30 +1,30 @@
-
 <template>
+
     <div class="form-check">
-        <input
-            type="checkbox"
-            v-model="model"
-            :id="$attrs.id"
-            :required="$attrs.required"
-            :disabled="$attrs.disabled"
-            :class="$attrs.class"
-            class="form-check-input"
-        />
-        <label :for="$attrs.id" class="form-check-label">
-            <slot name="label">{{ label }}</slot>
+        <input  @input="(event) => $emit('update:checked', event.target.checked)" type="checkbox" 
+        :checked="checked" :id="fieldId" class="form-check-input" />
+        <label :for="fieldId" class="form-check-label">            
+            <slot>
+                {{ label }}
+            </slot>  
         </label>
     </div>
 </template>
-
-<script setup>
-    const model = defineModel({
-        type: Boolean,
-        required: true,
-    });
-    const props = defineProps({
-        label: {
-        type: String,
-        required: true,
+  
+<script>
+export default {
+    props: {
+        label: String,
+        fieldId: {
+            type: String,
+            required: true,
         },
-    });
+        checked: {
+            type: Boolean,
+        },
+    },
+    emits:['update:checked']
+};
 </script>
+  
+<style></style>
