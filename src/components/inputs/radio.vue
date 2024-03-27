2b09b06ed081ffd454cc349a6da6c3bdd12add30
Yuan Li <liyuan.montreal@gmail.com>
2024-03-27 16:41:42 -0400

    add radio component

---
+++ src/components/inputs/radio.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="scbd-common radio form-check">   
+        <input type="radio" :id="$attrs.id"  v-model="model" :value="$attrs.value"
+            :required="$attrs.required"
+            :disabled="$attrs.disabled"
+            :class="$attrs.class"
+            class="form-check-input"/>
+        <label :for="$attrs.id" class="form-check-label">
+            <slot name="label">{{ label }}</slot>
+        </label> 
+    </div>
+</template>
+<script setup>
+    const model = defineModel({ type: Boolean,required: true });
+    const props = defineProps({ label: { type: String,  required: true }});
+</script>
