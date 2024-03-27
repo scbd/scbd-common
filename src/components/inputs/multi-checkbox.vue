1d5ad7517f1d0fa478dc15cf1bdbff80ad72d78c
Yuan Li <liyuan.montreal@gmail.com>
2024-03-27 16:06:26 -0400

    change to vue style multi-checkbox

--- src/components/inputs/multi-checkbox.vue
+++ src/components/inputs/multi-checkbox.vue
@@ -1,25 +1,19 @@
 <template>
-    <div class="km-multi-checkbox flex flex-col items-start justify-center w-64 border-2 p-8 rounded-lg">
-      <checkbox :id="option[props.optionValueField] + makeSmallUid()" v-for="option in options" :key="option" 
-            v-model = "checkStatus[option[props.optionValueField]] "         
-            :label = "lstring(option[props.optionTitleField])" 
-            @update:modelValue="check(option.identifier, $event)"
-        />           
-
+    <div class="km-multi-checkbox flex flex-col items-start justify-center w-64 border-2 p-8 rounded-lg">     
+        <checkbox :id="option[props.optionValueField] + makeSmallUid()" v-for="option in options" :key="option"  v-model="model"
+            :value = "{[props.optionValueField] :option[props.optionValueField]}"                 
+            :label = "lstring(option[props.optionTitleField])"            
+        />  
     </div>
-    checkStatus {{ checkStatus }}<br/>
-    updatedValue: {{updatedValue  }}<br/>
   </template>
   
 <script setup>
     import checkbox from "./checkbox.vue";  
     import { makeSmallUid }   from '@/services/util/index'
-    import { ref, defineEmits , defineProps, computed , defineModel} from "vue";
-    import { lstring } from '@/services/filters/lstring'
+    import {  defineProps,  defineModel} from "vue";
+    import { lstring } from '@/services/filters/lstring'; 
 
-
-    const model = defineModel({ type: Array,required: true });  
-    const emit = defineEmits(["update:modelValue"]);
+    const model = defineModel({ type: Array,required: true });   
 
     const props = defineProps({       
         options: { type: Array, required: true,
@@ -36,54 +30,5 @@
         optionValueField: { type:String, default:'identifier'},
         optionTitleField: { type:String, default:'title' }
     });  
-    
-    const check = (optionId, checked) => {  
-          model.value = updatedValue;
-    }
-
-    let updatedValue = computed(()=>{
-  
-
-        const newValue = [];
-        for (const key in checkStatus.value) {
-            if (checkStatus.value[key]){
-                newValue.push({[props.optionValueField] : key});  
-            }
-            //console.log(`${key}: ${checkStatus[key]}`);
-            }
-        return newValue;
-
-        //let updatedValue = [...model.value||[]];
-        //change {"en":true}--> { "identifier":en}
-
-        // if (checkStatus[optionId]) {          
-        //         updatedValue.push({[props.optionValueField] : optionId});       
-            
-        // } else {
-        //     index = updatedValue.indexOf({[props.optionValueField] :optionId});
-        //     updatedValue.splice(index, 1);
-        //     //updatedValue.splice(updatedValue.indexOf({[props.optionValueField] :optionId}), 1);
-       
-        // }
-        // return updatedValue;
-    }); 
-
-
-    // format: {"en":true, "cn":false}
-    let checkStatus = computed(()=>{ 
-        const list = ref({});
-        if (model.value){
-            for (const option of props.options) {
-            const id = option.identifier;
-            if (model.value.some(item => item.identifier === id)) { 
-                list.value [id]= true;   
-            } 
-            }         
-            return list.value;
-        }
-        else{
-            return {};
-        }
-    })
 </script>
   
