0c714d31849388ecd7e3f91432d8fd65e7620aff
Blaise Fonseca <blaise.fonseca@un.org>
2024-03-27 20:07:51 -0400

    Simply used model directly

--- src/components/selectors/km-government.vue
+++ src/components/selectors/km-government.vue
@@ -1,48 +1,36 @@
 <template> 
     <div class="scbd-common km-government">
         <multi-selector
-            v-model="selectedGovernment.identifier"
+            v-model="model.identifier"
             class="validationClass"
             label="name"
             track-by="code"
             value-key="code"
             :placeholder="$attrs.placeholder || t('government')"
             :options="countryList"
-            :disabled="!countryList || $attrs.disabled"    
-            @update:modelValue="onChange" >
+            :disabled="!countryList || $attrs.disabled" >
         </multi-selector>  
     </div>
 </template>
 
 <!-- TODO: add i18n support -->
 <script setup >
-    import { ref , computed , defineModel } from 'vue'
+    import { computed, defineModel } from 'vue'
     import multiSelector from "../controls/multi-selector.vue"
-    import { useI18n} from '../../services/composables/i18n';
+    import { useI18n } from '../../services/composables/i18n';
     import { useCountries }    from '@/services/composables/countries';   
     
-    const model = defineModel({ type: Array,required: true, default: {identifier:undefined}});  
-    const emit  = defineEmits(['update:modelValue']);
-
+    const model = defineModel({ type: Array,required: true, 
+                                default: {identifier:undefined}
+                            });  
+    
     const countries = useCountries ();   
     const {t, locale }   = useI18n();  
-    const selectedGovernment = ref({identifier:undefined});
 
-    const countryList   = computed(()=>{      
-        if(!countries?.length)
-             return [];
-        const mapCountries = countries.map(e=>{            
+    const countryList   = computed(()=>{  
+        return (countries||[]).map(e=>{            
             return { name : e.name[locale], code : e.code?.toLowerCase()}
         })
-        return mapCountries;
     })  
 
-    function onChange(code){
-         emit('update:modelValue', { identifier : code })
-    }  
-    
 </script>
-
-<style scoped>
-
-</style>
--- src/components/selectors/preview.vue
+++ src/components/selectors/preview.vue
@@ -2,27 +2,23 @@
 <template>
     <div>
         <h3>CBD selectors components</h3>
-         <preview-component card-header="language selector">
+        <preview-component card-header="Language selector">
             <template #left>
                 <km-language v-model="selectedItems"/> 
                 selected languages: {{ selectedItems }}        
             </template>          
             <template #right>
-                <code>
-                    {{ ` <km-language v-model="selectedItems/>  ` }}
-                </code>
+                {{ ` <km-language v-model="selectedItems/>  ` }}
             </template>
-        </preview-component> 
+        </preview-component>
 
-        <preview-component card-header="government selector">
+        <preview-component card-header="Government selector">
             <template #left>
                 <km-government v-model="selectedItems"/>        
                 selected government: {{ selectedItems }}        
             </template>          
             <template #right>
-                <code>
-                    {{ `<km-government v-model="selectedItems"/>  ` }}
-                </code>
+                {{ `<km-government v-model="selectedItems"/>  ` }}
             </template>
         </preview-component> 
     </div>
@@ -34,7 +30,7 @@
     import kmLanguage from './km-language.vue'
     
     import { ref } from 'vue'
-    const selectedItems = ref([]);
+    const selectedItems = ref({ "identifier": "ag" });
 </script>
 
 <style lang="scss" scoped>
