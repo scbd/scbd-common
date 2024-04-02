4e5f1cba677372c72a346e4bb178b8c1ee973334
Yuan Li <liyuan.montreal@gmail.com>
2024-04-02 16:55:54 -0400

    spinner modal feature

---
+++ src/components/controls/spinner-modal.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="scbd-common spinner-modal" tabindex="-1" role="dialog" v-if="props.visible" >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">{{props.title|| t('processing')}}</h5>          
+            </div>
+            <div class="modal-body">
+                <slot>
+                    <spinner> </spinner>
+                    <span v-if="message">{{ message }}</span>
+                    <span v-if="!message">{{t('loading')}}...</span>
+                </slot>
+            </div>         
+          </div>
+        </div>
+    </div>
+</template>
+
+
+<script setup >
+    import { useI18n} from '../../services/composables/i18n';
+    import spinner from './spinner.vue';
+
+    const {t, locale } = useI18n();  
+
+    const props = defineProps({
+        visible : { type:Boolean, default:false},
+        title   : { type:String},
+        message : { type:String}
+    })
+</script>
+
