e66d13f1dd66bfc1f7c38fca5547a2009aafb09d
Yuan Li <65098066+liyuanmontreal@users.noreply.github.com>
2024-04-03 09:48:55 -0400

    [CHM323] - spinner (#68)
    use bootstrap to implement a simple spinner
    
    https://github.com/scbd/scbd-common/assets/65098066/d81194f2-9184-4e47-9351-30610b52cd2f
    
    ---------
    
    Co-authored-by: Blaise Fonseca <blaise.fonseca@un.org>

---
+++ src/components/controls/spinner.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="scbd-common spinner" :class="{'d-flex justify-content-center' : alignCenter}">
+        
+        <span :class="variant" role="status"></span>
+        <slot>
+            <span class="ms-2" v-if="message">{{ message }}</span>
+            <span class="ms-2" v-if="!message">{{t('loading')}}...</span>
+        </slot>
+    </div>
+</template>
+
+<script setup >
+    import { useAttrs, computed } from 'vue'
+    import { useI18n } from '../../services/composables/i18n';
+
+    const props = defineProps({ 
+        message : { type: String },
+        variant : { type: String, default:'spinner-border'}
+    })
+
+    const { t } = useI18n();  
+    const attrs = useAttrs();
+
+    const alignCenter = computed(()=>attrs.hasOwnProperty('center'));  
+
+</script>
