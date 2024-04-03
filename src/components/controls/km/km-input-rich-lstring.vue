fe49821168f0a022ba1f3e5b840a6f5c857803d4
Yuan Li <liyuan.montreal@gmail.com>
2024-04-03 16:14:11 -0400

    add emit handler in preview

--- src/components/controls/km/km-input-rich-lstring.vue
+++ src/components/controls/km/km-input-rich-lstring.vue
@@ -1,24 +1,5 @@
 <template>
-
-
     <div :id="`km-rich-lstring-${uid}`"> 
-      <!-- <CNav variant="tabs" role="tablist">
-        <CNavItem v-for="locale in locales" :key="locale" :id="`lstringTab-${uid}`">
-          <CNavLink
-            href="javascript:void(0);" :active="selectedLocale === locale" @click="onTabChange(locale)">      
-              {{lstring(getTerm(locale).title||locale)}}
-          </CNavLink>
-        </CNavItem>
-      </CNav> -->
-
-       <!-- <CTabContent>
-        <CTabPane role="tabpanel" :aria-labelledby="`tabContent-${locale}-${uid}`" v-for="locale in locales" :key="locale" 
-          :visible="selectedLocale === locale" :id="`lstringTabContent-${uid}`">       
-          <ck-editor v-if="selectedLocale==locale" v-model="binding[selectedLocale]" :identifier="identifier"
-              :locale="selectedLocale" @update:modelValue="onChange" @onFileUpload="onFileUpload"></ck-editor>     
-        </CTabPane>
-      </CTabContent> -->
-
       <ul class="nav nav-tabs">      
          <li class="nav-item"  v-for="locale in props.locales" :key="locale" :id="`lstringTab-${uid}`">           
             <a class="nav-link " aria-current="page"  href="javascript:void(0);" :active="selectedLocale === locale" @click="onTabChange(locale)">      
@@ -26,34 +7,26 @@
             </a>
         </li>        
       </ul>
-      <br/>
-
-
-      <div :aria-labelledby="`tabContent-${locale}-${uid}`" v-for="locale in locales" :key="locale"  :visible="selectedLocale === locale" :id="`lstringTabContent-${uid}`">       
+    
+      <div class="mt-2" :aria-labelledby="`tabContent-${locale}-${uid}`" v-for="locale in locales" :key="locale"  :visible="selectedLocale === locale" :id="`lstringTabContent-${uid}`">       
             <ck-editor v-if="selectedLocale==locale" v-model="binding[selectedLocale]" :identifier="identifier"
-                       :locale="selectedLocale" @update:modelValue="onChange" @onFileUpload="onFileUpload">
+                       :locale="selectedLocale"  :config="allPluginsConfig" @update:modelValue="onChange" @onFileUpload="onFileUpload"  >
             </ck-editor>     
         </div> 
     </div>
-
-  </template>
-
-  
+  </template>  
 <script setup>
-    //import $ from 'jquery';
-    //TODO: replace coure-ui controls with boostrap controls
-    //TODO: remove code releated to userPreferencesStore and instead raise event for the parent to handle the userPreferencesStore -->
-    //import { makeUid } from '@coreui/utils/src'
+
 
     import { ref, watch, onMounted, computed} from 'vue';
     import {without} from 'lodash';
-    import ckEditor from '../km-ck-editor.vue';
+    import ckEditor from '../../inputs/ck-editor/ck-editor.vue';
     import { useThesaurus }    from '@/services/composables/thesaurus';  
     import { makeSmallUid }   from '@/services/util/index';
     import {lstring } from '../../../services/filters/lstring';   
     import { useKmStorage} from '../../../services/composables/storage';
-    //import { useUserPreferencesStore }    from '@/stores/userPreferences'; 
-
+    import { allPluginsConfig } from '../../inputs/ck-editor/all-plugins-config.js';
+  
     const model = defineModel({ type: Object, required: false, default:{}});
   
     const props = defineProps({
@@ -67,7 +40,6 @@
     const activeLocale = ref('');
     const uid = makeSmallUid();  
     //const tabPaneActiveKey =1;
-    //const userPreferencesStore = useUserPreferencesStore();
 
 
     watch(props.locales,  (newVal, oldVal) => {
@@ -103,30 +75,32 @@
     })
 
     const onChange=(value)=>{
-        const clean = useKmStorage().cleanDocument({...binding});
-     
+        const clean = useKmStorage().cleanDocument({...binding});     
         emit('update:modelValue', clean);
     }
+
     const onFileUpload=({file})=>{
         emit('onFileUpload', {file, locale : activeLocale.value});
     }
+
     const loadLanguages=()=>{   
         const thesaurusService  = useThesaurus();
         props.locales?.forEach(e=>{
             thesaurusService.loadDomainTerms (`lang-${e}`);
         });   
     }
+
     const  getTerm=(term)=>{     
         const thesaurusService  = useThesaurus();
         return thesaurusService.getDomainTerms(`lang-${term}`)||{};
     }
+
     const  onTabChange=(locale)=>{
         activeLocale.value = locale;
         // userPreferencesStore.setEditorActiveLanguageTab(locale);
         //TODO: add event handle for parents
         emit('userPreferences', locale);
    } 
-
 
     onMounted(() => {
         activeLocale.value =props.locales[0];        
