9dd393b90a702008e929c8d7c3e293f9dc7f7fc9
Yuan Li <liyuan.montreal@gmail.com>
2024-04-05 17:27:43 -0400

    add changeparentstatus func

--- src/components/controls/km/tree.vue
+++ src/components/controls/km/tree.vue
@@ -1,7 +1,7 @@
 <template> 
   <ul>
     <li v-if="root" >
-      <checkbox v-model="root.selected" @update:modelValue="onChange($event,root)" >        
+      <checkbox v-model="root.selected" @update:modelValue="onChange($event,root)" :id="root.termId" >        
         <template #label>
           {{ root.name}} 
         </template>
@@ -33,43 +33,72 @@
 
 
 
-  const onChange=(isSelected, parent)=>{  
-    //alert(isSelected);  
-    parent.selected=isSelected;
+  const onChange=(isSelected, currentNode)=>{  
+    // change status of current node
+    currentNode.selected=isSelected;
   
-    for (let i = 0; i < parent.narrowerTerms.length; i++) {  
-      changeChildrenStatus(parent.narrowerTerms[i],isSelected);  
+    // change status of all children and grandchildren
+    for (let i = 0; i < currentNode.narrowerTerms.length; i++) {  
+      changeChildrenStatus(currentNode.narrowerTerms[i],isSelected);  
     }
-    
-        //TODO: pass the id to this function
-    //@update:modelValue="onChange($event, item.id)
-    //onChange=(isSelected, id)=>{  
-    //if selected , select all the children, and half-selected parent
-    // if not selected,  unselect all the children, and unselect parent if it is the only child
+
+    // change status of parent    
+    for (let i = 0; i < currentNode.broaderTerms.length; i++) {  
+      changeParentStatus(currentNode.broaderTerms[i],isSelected);  
+    }
   }
 
-
-  const changeChildrenStatus=(identifier, isSelected)=>{
- 
-  
-    // const currentNode = props.data.find(object=>object.identifier ==identifier)
-    // currentNode.selected = isSelected;
-    // alert(currentNode.selected);
-    // for (let i = 0; i < currentNode.narrowerTerms.length; i++) {      
-    //   changeChildrenStatus(currentNode.narrowerTerms[i],isSelected);  
-    // } 
-    
-    
+  const changeChildrenStatus=(identifier, isSelected)=>{ 
     const currentNode = model.value.find(object=>object.identifier ==identifier)
     currentNode.selected = isSelected;
     //alert(currentNode.selected);
     for (let i = 0; i < currentNode.narrowerTerms.length; i++) {      
       changeChildrenStatus(currentNode.narrowerTerms[i],isSelected);  
-    } 
-    
+    }     
   }
 
- 
-  
+  const changeParentStatus=(identifier, isSelected)=>{ 
+    //if selected ,half-selected parent or select parent if all the children selected
+    //if not selected, half-selected parent or unselect parent if it is the only child
+    const currentNode = model.value.find(object=>object.identifier ==identifier)
+    if (isAllChildSelected(currentNode)=="true"){
+      currentNode.selected = true;
+    }
+    else if  (isAllChildSelected(currentNode)=="false"){
+      currentNode.selected = false;
+    }
+    else if (isAllChildSelected(currentNode)=="indeterminate"){
+      var checkbox = document.getElementById(currentNode.termId);
+      currentNode.selected = false;
+      //TODO: why it doesn't work?
+      checkbox.indeterminate = true;     
+    }
+   // currentNode.selected = isAllChildSelected(currentNode);      
+   
+    //TODO: add iterate for grandparent
+  }
+
+  const isAllChildSelected=(currentNode)=>{    
+    const childNumber = currentNode.narrowerTerms.length;
+    let selectedNumber = 0;    
+
+    for (let i = 0; i < currentNode.narrowerTerms.length; i++) {   
+      const id =currentNode.narrowerTerms[i];
+      const child = model.value.find(object=>object.identifier ==id)    
+      if (child.selected){
+        selectedNumber +=1;
+      }      
+    } 
+
+    if (selectedNumber ==0){
+      return "false";
+    }
+    else if (selectedNumber ==childNumber){
+      return "true";
+    }
+    else{   
+      return "indeterminate ";
+    }   
+  }
 
   </script>
