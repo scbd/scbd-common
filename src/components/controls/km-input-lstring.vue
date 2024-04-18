f7e8473f7394c437da9f26c2b62a05b2b6ef30fa
Yuan Li <65098066+liyuanmontreal@users.noreply.github.com>
2024-04-18 11:15:57 -0400

    [CHN336]-add textarea support for km-input-lstring (#72)
    add one props rows for distinguish  show input or textarea
        rows=1, show input
        rows>1, show textarea
    change examples in preview page,
       show modelValue below the example
       add one example for textarea
    <img width="806" alt="image"
    src="https://github.com/scbd/scbd-common/assets/65098066/03acb84a-0f6f-4aeb-8b56-b159bff4ac12">

--- src/components/controls/km-input-lstring.vue
+++ src/components/controls/km-input-lstring.vue
@@ -1,30 +1,12 @@
 <template>
-  <div :id="$attrs.id" class="scbd-controls km-input-lstring" :class="$attrs.class">
+  <div class="scbd-controls km-input-lstring" >
     <slot></slot>
-    <div
-      class="input-group mb-1"
-      v-for="locale in locales"
-      :key="locale"
-      :class="`km-input-lstring-${locale}`"
-    >
-      <input
-        type="text"
-        class="form-control"
-        aria-describedby="basic-addon2"
-        v-model="model[locale]"
-        :dir="locale == 'ar' ? 'rtl' : 'ltr'"
-        @input="$emit('update:modelValue', model)"
-      />
-
+    <div class="input-group mb-1" v-for="locale in locales" :key="locale" :class="`km-input-lstring-${locale}`">
+      <input     v-if="rows==1" type="text"  class="form-control" :placeholder="placeholder" aria-describedby="lstring-text"     v-model="model[locale]" :dir="locale == 'ar' ? 'rtl' : 'ltr'" @input="$emit('update:modelValue', model)"/> 
+      <textarea  v-if="rows>1"  :rows=rows   class="form-control" :placeholder="placeholder" aria-describedby="lstring-textarea" v-model="model[locale]" :dir="locale == 'ar' ? 'rtl' : 'ltr'" @input="$emit('update:modelValue', model)"/>      
+    
       <div class="input-group-append">
-        <button
-          class="btn btn-outline-primary"
-          type="button"
-          id="basic-addon2"
-          :data-bs-toggle="'tooltip'"
-          :data-bs-placement="'top'"
-          :title="getTerm(locale).title"
-        >
+        <button class="btn btn-outline-primary" type="button" id="basic-addon2" :data-bs-toggle="'tooltip'" :data-bs-placement="'top'" :title="getTerm(locale).title">
           {{ locale.toUpperCase() }}
         </button>
       </div>
@@ -33,39 +15,41 @@
 </template>
 
 <script setup>
-import { watch, onMounted } from "vue";
-import { without } from "lodash";
+    import { watch, onMounted } from "vue";
+    import { without } from "lodash";
 
-const model = defineModel({type:Object, required: true, default:{}})
-const props = defineProps({
-  locales: { type: Array, required: true },
-  disabled: { type: Boolean, default: false },
-});
+    const model = defineModel({type:Object, required: true, default:{}})
+    const props = defineProps({
+      locales:  { type: Array, required: true },
+      disabled: { type: Boolean, default: false },
+      rows:     { type: Number, required: false, default: 1 },
+      placeholder: {type: String, required: false }
+    });
 
-const loadLanguages = () => {
-  props.locales?.forEach((e) => {
-    //TODO: call thesaurus service API
-  });
-};
-
-const getTerm = (term) => {
-  //TODO: call thesaurus service API
-  return { title: term };
-};
-
-watch(()=>props.locales,
-  (newVal, oldVal) => {
-    const deleted = without(Object.keys(model.value), ...newVal);
-    if (deleted?.length) {
-      deleted.forEach((e) => {
-        model.value[e] = undefined;
+    const loadLanguages = () => {
+      props.locales?.forEach((e) => {
+        //TODO: call thesaurus service API
       });
-    }
-    loadLanguages();
-  }, {deep:true}
-);
+    };
 
-onMounted(() => {
-  loadLanguages();
-});
+    const getTerm = (term) => {
+      //TODO: call thesaurus service API
+      return { title: term };
+    };
+
+    watch(()=>props.locales,
+      (newVal, oldVal) => {
+        const deleted = without(Object.keys(model.value), ...newVal);
+        if (deleted?.length) {
+          deleted.forEach((e) => {
+            model.value[e] = undefined;
+          });
+        }
+        loadLanguages();
+      }, {deep:true}
+    );
+
+    onMounted(() => {
+      loadLanguages();
+    });
 </script>
